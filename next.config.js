1175448e60c83e62133b2e37c119088baca85712
wof <angel@watchoutfreedom.com>
2021-09-09 13:49:52 +0300

    Add svg plugin edition

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const withSvgr = require("next-svgr");
+
+
 module.exports = {
   reactStrictMode: true,
 }
+
+
+module.exports = withSvgr({
+  // your config for other plugins or the general next.js here...
+});
